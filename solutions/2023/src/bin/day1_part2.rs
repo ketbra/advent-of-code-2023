ab3696b20e10f355dcbb35cba071bdbf8e9415f9
Matthew Feinberg <feinberg@alum.mit.edu>
2023-12-01 12:42:19 -0500

    Simplified 2023 1b solution with greedy regex

--- solutions/2023/src/bin/day1_part2.rs
+++ solutions/2023/src/bin/day1_part2.rs
@@ -1,15 +1,9 @@
 use anyhow::Result;
 use std::collections::HashMap;
 
-fn str_rev(s: &str) -> String {
-    s.chars().rev().collect::<String>()
-}
-
 #[aoc::main]
 fn solve(input: &str) -> Result<u32> {
     let vals: Vec<String> = aoc::parse_list(input)?;
-
-    // println!("{:?}", vals);
 
     let num_map = HashMap::from([
         ("one", "1"),
@@ -25,22 +19,19 @@ fn solve(input: &str) -> Result<u32> {
 
     let mut answer = 0;
     for val in &vals {
-        let text = lazy_regex::regex_replace!(
-            r#"^[^0-9]*?(one|two|three|four|five|six|seven|eight|nine)"#,
-            val,
-            |_, name| num_map[name]
+        let (_, d1) = lazy_regex::regex_captures!(
+            r#"(\d|one|two|three|four|five|six|seven|eight|nine)"#,
+            val
         )
-        .to_string();
-        let text = lazy_regex::regex_replace!(
-            r#"^[^0-9]*?(enin|thgie|neves|xis|evif|ruof|eerht|owt|eno)"#,
-            str_rev(&text).as_str(),
-            |_, name| num_map[str_rev(name).as_str()]
+        .unwrap();
+        let (_, d2) = lazy_regex::regex_captures!(
+            r#".*(\d|one|two|three|four|five|six|seven|eight|nine)"#,
+            val
         )
-        .to_string();
-        let text = str_rev(&text);
-        let text = lazy_regex::regex_replace_all!(r#"[^0-9]"#, &text, "").to_string();
-        let chars: Vec<char> = text.chars().collect();
-        let num = format!("{}{}", chars[0], chars[chars.len() - 1]);
+        .unwrap();
+        let d1 = if d1.len() > 1 { num_map[d1] } else { d1 };
+        let d2 = if d2.len() > 1 { num_map[d2] } else { d2 };
+        let num = format!("{}{}", d1, d2);
         answer += num.parse::<u32>()?;
     }
     Ok(answer)
