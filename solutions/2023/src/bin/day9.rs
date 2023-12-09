339cb0e79246aa02347db12af997cadaa2a4de8b
Matthew Feinberg <feinberg@alum.mit.edu>
2023-12-09 07:00:26 -0500

    Added 2023 day 9

---
+++ solutions/2023/src/bin/day9.rs
@@ -0,0 +1,56 @@
+use anyhow::Result;
+use itertools::Itertools;
+use std::collections::HashMap;
+
+#[aoc::main]
+fn solve(input: &str) -> Result<i64> {
+    let sequences = aoc::parse_list::<String>(input)?
+        .iter()
+        .map(|x| {
+            x.split_whitespace()
+                .map(|s| s.parse::<i64>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    println!("{:?}", sequences);
+
+    let mut answer = 0;
+    for sequence in sequences {
+        let mut last_nums = Vec::new();
+        let mut sequence = sequence;
+        while !sequence.iter().all(|x| *x == 0) {
+            let next_sequence = get_next_sequence(&sequence);
+            last_nums.push(*sequence.last().unwrap());
+            sequence = next_sequence;
+        }
+
+        let mut next_num = 0;
+        for x in last_nums.iter().rev() {
+            next_num += *x;
+        }
+
+        answer += next_num;
+    }
+
+    Ok(answer)
+}
+
+fn get_next_sequence(sequence: &[i64]) -> Vec<i64> {
+    (0..(sequence.len() - 1))
+        .map(|i| sequence[i + 1] - sequence[i])
+        .collect_vec()
+}
+
+fn tests() -> anyhow::Result<()> {
+    let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+";
+
+    let solution = solve(input)?;
+
+    assert_eq!(solution, 114);
+
+    Ok(())
+}
---
+++ solutions/2023/src/bin/day9_part2.rs
@@ -0,0 +1,54 @@
+use anyhow::Result;
+use itertools::Itertools;
+use std::collections::HashMap;
+
+#[aoc::main]
+fn solve(input: &str) -> Result<i64> {
+    let sequences = aoc::parse_list::<String>(input)?
+        .iter()
+        .map(|x| {
+            x.split_whitespace()
+                .map(|s| s.parse::<i64>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let mut answer = 0;
+    for sequence in sequences {
+        let mut first_nums = Vec::new();
+        let mut sequence = sequence;
+        while !sequence.iter().all(|x| *x == 0) {
+            let next_sequence = get_next_sequence(&sequence);
+            first_nums.push(*sequence.first().unwrap());
+            sequence = next_sequence;
+        }
+
+        let mut next_num = 0;
+        for x in first_nums.iter().rev() {
+            next_num = *x - next_num;
+        }
+
+        answer += next_num;
+    }
+
+    Ok(answer)
+}
+
+fn get_next_sequence(sequence: &[i64]) -> Vec<i64> {
+    (0..(sequence.len() - 1))
+        .map(|i| sequence[i + 1] - sequence[i])
+        .collect_vec()
+}
+
+fn tests() -> anyhow::Result<()> {
+    let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+";
+
+    let solution = solve(input)?;
+
+    assert_eq!(solution, 2);
+
+    Ok(())
+}
