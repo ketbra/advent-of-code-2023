529d89b1b93210248641448bc29da49297962b2c
Matthew Feinberg <feinberg@alum.mit.edu>
2023-12-28 01:58:54 -0500

    Updated 2023 day 24 part 2.  Finally figured out how to get my Python z3 solution working in Rust.  The key was that I need to use z3::ast::Ast for the _eq method to be available

--- solutions/2023/src/bin/day24_part2.rs
+++ solutions/2023/src/bin/day24_part2.rs
@@ -1,10 +1,8 @@
 use anyhow::Result;
 use itertools::Itertools;
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::collections::VecDeque;
-use z3::ast::Real;
-use z3::{Config, Context, Solver};
+use z3::ast::{Ast, Int, Real};
+
+// Note.  If we don't use z3::ast::Ast, then the _eq method is not available
 
 #[derive(Debug, Clone, PartialEq)]
 struct HailStone {
@@ -34,58 +32,61 @@ fn solve(input: &str) -> Result<usize> {
         })
         .collect_vec();
 
-    let cfg = z3::Config::new();
-    let ctx = z3::Context::new(&cfg);
-    let solver = z3::Solver::new(&ctx);
+    let ctx = z3::Context::new(&z3::Config::new());
+    let s = z3::Solver::new(&ctx);
 
-    let x_0 = Real::fresh_const(&ctx, "x__0");
-    let y_0 = Real::fresh_const(&ctx, "y__0");
-    let z_0 = Real::fresh_const(&ctx, "z__0");
-    let vx_0 = Real::fresh_const(&ctx, "vx__0");
-    let vy_0 = Real::fresh_const(&ctx, "vy__0");
-    let vz_0 = Real::fresh_const(&ctx, "vz__0");
-
+    // Create two real variables
+    let x_0 = Real::new_const(&ctx, "x__0");
+    let y_0 = Real::new_const(&ctx, "y__0");
+    let z_0 = Real::new_const(&ctx, "z__0");
+    let vx_0 = Real::new_const(&ctx, "vx__0");
+    let vy_0 = Real::new_const(&ctx, "vy__0");
+    let vz_0 = Real::new_const(&ctx, "vz__0");
     let ts = hailstones
         .iter()
         .enumerate()
-        .map(|(i, _)| Real::fresh_const(&ctx, &format!("t__{}", i + 1)))
+        .map(|(i, _)| Real::new_const(&ctx, format!("t__{}", i + 1)))
         .collect_vec();
 
     for (i, h) in hailstones.iter().enumerate() {
-        // solver.assert(x_0 + vx_0 * ts[i] == h.position[0] + h.velocity[0] * ts[i]);
-        solver.assert(&x_0 + &vx_0 == 5);
+        let p = h
+            .position
+            .iter()
+            .map(|x| Int::from_i64(&ctx, *x as i64).to_real())
+            .collect_vec();
+
+        let v = h
+            .velocity
+            .iter()
+            .map(|x| Int::from_i64(&ctx, *x as i64).to_real())
+            .collect_vec();
+
+        s.assert(&((&x_0 + &vx_0 * &ts[i])._eq(&(&p[0] + &v[0] * &ts[i]))));
+        s.assert(&((&y_0 + &vy_0 * &ts[i])._eq(&(&p[1] + &v[1] * &ts[i]))));
+        s.assert(&((&z_0 + &vz_0 * &ts[i])._eq(&(&p[2] + &v[2] * &ts[i]))));
     }
-    // ctx.
-    // let x_0 = ctx.real_const("x__0");
-    // let y_0 = ctx.real_const("y__0");
-    // let z_0 = ctx.real_const("z__0");
-    // let vx_0 = ctx.real_const("vx__0");
-    // let vy_0 = ctx.real_const("vy__0");
-    // let vz_0 = ctx.real_const("vz__0");
 
-    // let mut answer = 0;
-    // let throw = HailStone {
-    //     position: vec![
-    //         390970075767404.0,
-    //         226195425713131238783606819606.0,
-    //         466980968436673869995870521057.0,
-    //     ],
-    //     velocity: vec![
-    //         -147.0,
-    //         59.0 * 226195425713131238783606819606.0,
-    //         -107.0 * 466980968436673869995870521057.0,
-    //     ],
-    // };
-    // for h1 in &hailstones {
-    //     if let Some((x, y, t1, t2)) = calculate_intersection(h1, &throw) {
-    //         println!("{t1},{t2}");
-    //         if x >= min && x <= max && y >= min && y <= max && t1 >= 0.0 && t2 >= 0.0 {
-    //             answer += 1;
-    //         }
-    //     }
-    // }
+    // Check if the solver is satisfiable
+    let mut answer = 0;
+    if s.check() == z3::SatResult::Sat {
+        // Get the model
+        let model = s.get_model().unwrap();
 
-    let answer = 4;
+        // Get the values of x and y from the model
+        let (x_value, x_denominator) = model.eval(&x_0, true).unwrap().as_real().unwrap();
+        let (y_value, y_denominator) = model.eval(&y_0, true).unwrap().as_real().unwrap();
+        let (z_value, z_denominator) = model.eval(&z_0, true).unwrap().as_real().unwrap();
+
+        // Make sure the solution values are integers
+        assert_eq!(x_denominator, 1);
+        assert_eq!(y_denominator, 1);
+        assert_eq!(z_denominator, 1);
+
+        answer = (x_value + y_value + z_value) as usize;
+    } else {
+        println!("No solution found");
+    }
+
     Ok(answer)
 }
 
